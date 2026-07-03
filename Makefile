bc38675280ed1915e7b96960e1d2f3623b4894fe
Avalon <210815572+Avalon96@users.noreply.github.com>
2026-07-03 12:27:25 +0300

    Split config into config.py; Makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,35 @@
-install
+# Windows
 
-run
+# PYTHON = python3
+PYTHON = python
+SRC = main.py
+CONFIG = default_config.txt
 
-debug
+FLAKE8 = flake8 .
+MYPY = mypy . --warn-return-any \
+--warn-unused-ignores \
+--ignore-missing-imports \
+--disallow-untyped-defs \
+--check-untyped-defs
+MYPY_STRICT = mypy . --strict
 
-clean
+install:
+	$(PYTHON) -m pip install -r requirements.txt
 
-lint
+run:
+	$(PYTHON) $(SRC) $(CONFIG)
 
-lint-strict
+debug:
+	$(PYTHON) -m pdb $(SRC) $(CONFIG)
+
+clean:
+	rmdir /s /q __pycache__
+	rmdir /s /q .mypy_cache
+# 	rm -rf __pycache__ .mypy_cache
+
+lint:
+	$(FLAKE8)
+	$(MYPY)
+
+lint-strict:
+	$(MYPY_STRICT)
--- main.py
+++ main.py
@@ -1,62 +1,16 @@
-import sys
-
-CONFIG_KEYS_INT: tuple[str, str] = ("WIDTH", "HEIGHT")
-CONFIG_KEYS_COORD: tuple[str, str] = ("ENTRY", "EXIT")
-CONFIG_KEYS_STR: tuple[str] = ("OUTPUT_FILE",)
-CONFIG_KEYS_BOOL: tuple[str] = ("PERFECT",)
-MANDATORY_CONFIG_KEYS: dict[tuple[str, ...], type] = {
-    CONFIG_KEYS_INT: int,
-    CONFIG_KEYS_COORD: tuple,
-    CONFIG_KEYS_STR: str,
-    CONFIG_KEYS_BOOL: bool
-}
-
-
-def read_config_file(file_path: str) -> dict[str, str | int]:
-    with open(file_path, 'r') as file:
-        config: dict[str, str | int] = {}
-        for line in file:
-            line = line.strip()
-            if not line or line.startswith('#'):
-                continue
-            key: str = line.split('=')[0].strip()
-            value: str | int | tuple = line.split('=')[1].strip()
-            if key in CONFIG_KEYS_INT:
-                config[key] = int(value)
-            elif key in CONFIG_KEYS_COORD:
-                config[key] = tuple(map(int, value.split(',')))
-            elif key in CONFIG_KEYS_BOOL:
-                config[key] = value.lower() == 'true'
-            else:
-                config[key] = value
-    return config
-
-
-def validate_config(config: dict[str, str | int]):
-    for key_group, expected_type in MANDATORY_CONFIG_KEYS.items():
-        for key in key_group:
-            # print(f"{key} = {config.get(key)}")
-            if key not in config:
-                raise ValueError(f"Missing mandatory configuration key: {key}")
-            if not isinstance(config[key], expected_type):
-                raise ValueError(
-                    f"Invalid type for configuration key: {key}. "
-                    f"Expected {expected_type.__name__}, "
-                    f"got {type(config[key]).__name__}"
-                )
-
-
-def validate_parameters() -> str:
-    if len(sys.argv) != 2:
-        raise ValueError("Usage: python main.py <config_file_path>")
-    return sys.argv[1]
+from config import (
+    read_config_file,
+    validate_config,
+    validate_parameters
+)
 
 
 def main() -> None:
     config_file_path: str = validate_parameters()
     config: dict[str, str | int] = read_config_file(config_file_path)
     validate_config(config)
-    # print(config)
+    # for key, value in config.items():
+    #     print(f"{key}: {value}")
 
 
 if __name__ == "__main__":
